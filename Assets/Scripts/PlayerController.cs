73e59133645ae3917cf0a59ffbc772412c913b47
Arda Zeytin <ardazeytin@outlook.com>
2017-04-09 13:00:48 +0300

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody playerRigidbody;
+    private float runSpeed = 10.0f;
+    private int lane = 0;
+    private float velocity;
+    private float smoothValue = 0.3f;
+    private int score;
+    public Text scoreText;
+
+    private float jumpForce = 4.0f;
+
+
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        score = 0;
+	}
+	
+	void Update ()
+    {
+        transform.Translate(0, 0, runSpeed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (lane > -1)
+            {
+                lane--;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (lane < 1)
+            {
+                lane++;
+            }
+        }
+
+        Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.SmoothDamp(newPosition.x,lane,ref velocity,smoothValue);
+        transform.position = newPosition;
+
+    }
+    private void FixedUpdate()
+    {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (transform.position.y < 0.6f)
+            {
+                anim.SetTrigger("isJumping");
+                playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("coin"))
+        {
+            score++;
+            scoreText.text = "Score: " + score.ToString();
+            print(score);
+            Instantiate(other.transform.GetChild(0), other.transform.position, Quaternion.identity);
+            //other.gameObject.SetActive(false);
+
+        }
+
+        if (other.CompareTag("obstacle"))
+        {
+            StartCoroutine(SlowDownPlayer());
+            Instantiate(other.transform.GetChild(0), other.transform.position, Quaternion.identity);
+            print("Speed: " + runSpeed);
+        }
+    }
+
+    IEnumerator SlowDownPlayer()
+    {
+        runSpeed = 5;
+        yield return new WaitForSeconds(3);
+        runSpeed = 10;
+    }
+
+}
---
+++ Assets/Scripts/StuffSpawner.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffSpawner : MonoBehaviour {
+
+	public Transform[] stuffSpawnPoints;
+	public GameObject coin;
+	public GameObject obstacle;
+    
+	void Start () 
+	{
+       
+	}
+
+    void Update () 
+	{
+        
+	}
+
+    GameObject SpawnObject()
+    {
+        int obstacleOrCoinChance = Random.Range(0, 2);
+        if (obstacleOrCoinChance == 0)
+        {
+            return coin;
+        }
+        else
+        {
+            return obstacle;
+        }
+    }
+
+    Vector3 RandomSpawnLaneValue()
+    {
+        int temp = Random.Range(0, 3);
+        if (temp == 0)
+        {
+            return new Vector3(-1,0,154); //leftlane
+        }
+        else if(temp == 1)
+        {
+            return new Vector3(1,0,154); //rightlane
+        }
+        else
+        {
+            return new Vector3(0, 0, 154); //midlane
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        for (int i = 0; i < stuffSpawnPoints.Length; i++)
+        {
+            GameObject tempSpawnObject = SpawnObject();
+            if (tempSpawnObject == coin)
+            {
+                Instantiate(tempSpawnObject, stuffSpawnPoints[i].position + RandomSpawnLaneValue(), Quaternion.identity);
+            }
+
+            if (tempSpawnObject == obstacle)
+            {
+                RaycastHit hitDown = new RaycastHit();
+                Ray downRay = new Ray(obstacle.transform.position, Vector3.down);
+                Vector3 targetPos = new Vector3();
+                targetPos.y = hitDown.point.y + 0.1f;
+                targetPos.x = stuffSpawnPoints[i].position.x;
+                targetPos.z = stuffSpawnPoints[i].position.z;
+                Instantiate(tempSpawnObject, targetPos + RandomSpawnLaneValue(), Quaternion.Euler(90,0,0));
+            }
+            
+        }
+    }
+}
